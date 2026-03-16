ff9e96e77435c54b14bd6a843672a4cdeb2b70e8
YFJ CENTER <yfjcenter@gmail.com>
2026-03-16 13:27:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4820738614-892a7",
+  "appId": "1:428830126173:web:54758400d10a0e1193ddb4",
+  "apiKey": "AIzaSyC_GmaR2TjUeaSQCniAPTcvAVTGvap0zdU",
+  "authDomain": "studio-4820738614-892a7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "428830126173"
+};
